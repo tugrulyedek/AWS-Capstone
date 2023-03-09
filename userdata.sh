06bc61b18bd83e292d6dd13b695a6150266d3b88
tugrulyedek <tugrul.gdk@gmail.com>
2023-03-09 12:31:11 +0300

    Chancing github account

--- userdata.sh
+++ userdata.sh
@@ -4,12 +4,12 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN=xxx
-git clone https://$TOKEN@github.com/tugrulgdk/my-aws-capstone-project.git
-cd /home/ubuntu/my-aws-capstone-project
+git clone https://$TOKEN@github.com/tugrulyedek/AWS-Capstone.git
+cd /home/ubuntu/AWS-Capstone
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/my-aws-capstone-project/src
+cd /home/ubuntu/AWS-Capstone/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
